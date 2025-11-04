4b4105de6a5e86f580aa374173acc8b61b6542a2
Tyler452 <tpesl@csumb.edu>
2025-11-03 18:05:48 -0800

    CMake build

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/tyler/CST-426/Chess_assignment-main
+# Build directory: C:/Users/tyler/CST-426/Chess_assignment-main/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
